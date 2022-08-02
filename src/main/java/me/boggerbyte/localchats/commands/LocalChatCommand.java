41e08fda5ca2d05dbbf440a446694eb4d0230158
Nick Troshnev <nik.troshnev@gmail.com>
2022-08-02 19:41:56 +0200

    Implemented /local and /global chat commands

---
+++ src/main/java/me/boggerbyte/localchats/commands/LocalChatCommand.java
@@ -0,0 +1,33 @@
+package me.boggerbyte.localchats.commands;
+
+import me.boggerbyte.localchats.chat_executor.LocalChatExecutor;
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class LocalChatCommand implements CommandExecutor {
+    private final LocalChatExecutor localChatExecutor;
+
+    public LocalChatCommand(LocalChatExecutor localChatExecutor) {
+        this.localChatExecutor = localChatExecutor;
+    }
+
+    @Override
+    public boolean onCommand(
+            @NotNull CommandSender sender,
+            @NotNull Command command,
+            @NotNull String label,
+            @NotNull String[] args
+    ) {
+        if (args.length == 0) return false;
+        if (!(sender instanceof Player player)) return false;
+
+        var message = Component.text(String.join(" ", args));
+        localChatExecutor.onMessage(player, message);
+
+        return true;
+    }
+}
