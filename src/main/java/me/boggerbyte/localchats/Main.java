bc2f940762f56a3e5434db41cbd6211ff228d13b
Nick Troshnev <nik.troshnev@gmail.com>
2022-08-02 19:42:47 +0200

    Implemented /local and /global chat commands; Add flexible config

--- src/main/java/me/boggerbyte/localchats/Main.java
+++ src/main/java/me/boggerbyte/localchats/Main.java
@@ -1,9 +1,11 @@
 package me.boggerbyte.localchats;
 
-import me.boggerbyte.localchats.commands.ShoutCommand;
+import me.boggerbyte.localchats.chat_executor.GlobalChatExecutor;
+import me.boggerbyte.localchats.chat_executor.LocalChatExecutor;
+import me.boggerbyte.localchats.commands.GlobalChatCommand;
+import me.boggerbyte.localchats.commands.LocalChatCommand;
 import me.boggerbyte.localchats.listeners.ChatListener;
 import me.boggerbyte.localchats.utils.Logger;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,12 +18,18 @@ public final class Main extends JavaPlugin {
     @Override
     public void onLoad() {
         saveDefaultConfig();
-
         var config = getConfig();
         var configRequiredFields = new String[]{
-                "max-x-distance",
-                "max-y-distance",
-                "max-z-distance",
+                "local-chat-radius",
+                "local-chat.layout",
+                "local-chat.on-command",
+                "local-chat.on-prefix",
+                "local-chat.prefix",
+                "global-chat.layout",
+                "global-chat.on-command",
+                "global-chat.on-prefix",
+                "global-chat.prefix",
+                "default-chat"
         };
         Arrays.stream(configRequiredFields)
                 .filter(field -> !config.contains(field, true))
@@ -32,19 +40,27 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         var config = getConfig();
 
-        PlayerFinder playerFinder = location -> {
-            var nearByEntities = location.getNearbyEntities(
-                        config.getDouble("max-x-distance"),
-                        config.getDouble("max-y-distance"),
-                        config.getDouble("max-z-distance"));
-            return nearByEntities.stream()
-                .filter(e -> e instanceof Player)
-                .map(e -> (Player) e)
-                .toList();
-        };
-        getServer().getPluginManager().registerEvents(new ChatListener(playerFinder), this);
+        var localChatExecutor = new LocalChatExecutor(config.getString("local-chat.layout"), config.getDouble("local-chat-radius"));
+        var globalChatExecutor = new GlobalChatExecutor(config.getString("global-chat.layout"));
 
-        Objects.requireNonNull(getCommand("shout")).setExecutor(new ShoutCommand());
+        var chatListener = new ChatListener(
+                localChatExecutor,
+                globalChatExecutor,
+                config.getBoolean("local-chat.on-prefix"),
+                config.getBoolean("global-chat.on-prefix"),
+                config.getString("local-chat.prefix"),
+                config.getString("global-chat.prefix"),
+                config.getString("default-chat"));
+        getServer().getPluginManager().registerEvents(chatListener, this);
+
+        if (config.getBoolean("local-chat.on-command")) {
+            var localChatCommand = new LocalChatCommand(localChatExecutor);
+            Objects.requireNonNull(getCommand("local")).setExecutor(localChatCommand);
+        }
+        if (config.getBoolean("global-chat.on-command")) {
+            var globalChatCommand = new GlobalChatCommand(globalChatExecutor);
+            Objects.requireNonNull(getCommand("global")).setExecutor(globalChatCommand);
+        }
     }
 
     public static Plugin getInstance() {
